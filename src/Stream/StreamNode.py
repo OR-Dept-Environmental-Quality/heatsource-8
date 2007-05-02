e97131481c87dd339258991ea070b1b2f2c50d1c
Dan Turner <turner.daniel@deq.state.or.us>
2007-05-02 23:51:13 +0000

    Some additional accounting for effective shade calculation and output
    git-svn-id: file://deqhq1/tmdl/Subversion/HeatSource/HeatSource@106 ffc44b20-e41f-394f-8689-23e218f545e0

--- src/Stream/StreamNode.py
+++ src/Stream/StreamNode.py
@@ -28,6 +28,7 @@ class StreamNode(StreamChannel):
              "Delta_T", # Current temperature calculated from only local fluxes
              "T", "T_prev", # Current and previous stream temperature
              "Flux" # Dictionary to hold heat flux values
+             "Evap_Rate"  #Evaportion in m/s
              ]
         # Set all the attributes to bare lists, or set from the constructor
         for attr in s:
@@ -38,7 +39,8 @@ class StreamNode(StreamChannel):
             setattr(self, attr, TimeList())
         self.Flux = {"Direct":  [0]*8,
                      "Diffuse": [0]*8,
-                     "Solar":   [0]*8}
+                     "Solar":   [0]*8,
+                     "Solar_daily_sum": [0]*5}
         self.Topo = {"E": None,
                      "S": None,
                      "W": None}
@@ -212,12 +214,14 @@ class StreamNode(StreamChannel):
         # Make all math functions local to save time by preventing failed searches of local, class and global namespaces
         pi,exp,log10,log,sqrt = math.pi,math.exp,math.log10,math.log,math.sqrt
         sin,cos,tan,atan,radians = math.sin,math.cos,math.tan,math.atan,math.radians
-        # If it's night, we get out quick.
+        # If it's night, we get out quick.  TODO: would it save some run time to do this only once per night?
         if Altitude <= 0: #Nighttime
             Altitude = 0
             self.Flux["Direct"] = [0]*8
             self.Flux["Diffuse"] = [0]*8
             self.Flux["Solar"] = [0]*8
+            if Chronos.TheTime.hour == 1:
+                self.Flux["Solar_daily_sum"] = [0]*5   #reset for the new day
             return False #Nothing else to do, so ignore rest of method
 
         #########################################################
@@ -458,6 +462,10 @@ class StreamNode(StreamChannel):
         self.Flux["Solar"][5] = self.Flux["Diffuse"][5] + self.Flux["Direct"][5]
         self.Flux["Solar"][6] = self.Flux["Diffuse"][6] + self.Flux["Direct"][6]
         self.Flux["Solar"][7] = self.Flux["Diffuse"][7] + self.Flux["Direct"][7]
+        self.Flux["Solar_daily_sum"][1] += self.Flux["Solar"][1]
+        self.Flux["Solar_daily_sum"][4] += self.Flux["Solar"][4]
+
+
         return True
 
     def CalcConductionFlux(self):
@@ -578,17 +586,17 @@ class StreamNode(StreamChannel):
             if NetRadiation < 0:
                 NetRadiation = 0 #J/m2/s
             Ea = Wind_Function * (Sat_Vapor - Air_Vapor)  #m/s
-            Evap_Rate = ((NetRadiation * Delta / (P * LHV)) + Ea * Gamma) / (Delta + Gamma)
-            self.Flux["Evaporation"] = -Evap_Rate * LHV * P #W/m2
+            self.Evap_Rate = ((NetRadiation * Delta / (P * LHV)) + Ea * Gamma) / (Delta + Gamma)
+            self.Flux["Evaporation"] = -self.Evap_Rate * LHV * P #W/m2
             #Calculate Convection FLUX
             Bowen = Gamma * (self.T_prev - Air_T) / (Sat_Vapor - Air_Vapor)
             self.Flux["Convection"] = self.Flux["Evaporation"] * Bowen
         else:
             #===================================================
             #Calculate Evaporation FLUX
-            Evap_Rate = Wind_Function * (Sat_Vapor - Air_Vapor)  #m/s
+            self.Evap_Rate = Wind_Function * (Sat_Vapor - Air_Vapor)  #m/s
             P = 998.2 # kg/m3
-            self.Flux["Evaporation"] = -Evap_Rate * LHV * P #W/m2
+            self.Flux["Evaporation"] = -self.Evap_Rate * LHV * P #W/m2
             #Calculate Convection FLUX
             if (Sat_Vapor - Air_Vapor) <> 0:
                 Bowen = 0.61 * (Pressure / 1000) * (self.T_prev - Air_T) / (Sat_Vapor - Air_Vapor)
