d963f486f32750d18d3e84e5c9b0d52f0fa6be5e
Dan Turner <turner.daniel@deq.state.or.us>
2007-05-03 00:06:44 +0000

    git-svn-id: file://deqhq1/tmdl/Subversion/HeatSource/HeatSource@107 ffc44b20-e41f-394f-8689-23e218f545e0

--- src/Utils/Output.py
+++ src/Utils/Output.py
@@ -9,37 +9,41 @@ class Output(object):
         self.dt_out = dt_out
         self.reach = reach
         self.write_time = write_time
+        #In the dictionary below the value is a list, the first item will contain file information and the second
+        #item is a love note.  Still need to put in date created.
         self.files = {
-                        "Heat_Cond.txt": None,
-                        "Heat_Conv.txt": None,
-                        "Heat_Evap.txt": None,
-                        "Heat_SR1.txt": None,
-                        "Heat_SR4.txt": None,
-                        "Heat_SR6.txt": None,
-                        "Heat_TR.txt": None,
-                        "Hyd_DA.txt": None,
-                        "Hyd_DM.txt": None,
-                        "Hyd_Flow.txt": None,
-                        "Hyd_Froude.txt": None,
-                        "Hyd_Hyp.txt": None,
-                        "Hyd_Vel.txt": None,
-                        "Hyd_WT.txt": None,
-                        "Rate_Evap.txt": None,
-                        "Shade.txt": None,
-                        "Temp_H20.txt": None,
-                        "Temp_Sed.txt": None,
-                        "VTS.txt": None
+                        "Heat_Cond.txt": [None, "Streambed Conduction Flux (w/sq m)"],
+                        "Heat_Conv.txt": [None, "Convection Flux (w/sq m)"],
+                        "Heat_Evap.txt": [None, "Evaporation Flux (w/sq m)"],
+                        "Heat_SR1.txt": [None, "Potential Solar Radiation Flux (w/sq m)"],
+                        "Heat_SR4.txt": [None, "Surface Solar Radiation Flux (w/sq m)"],
+                        "Heat_SR6.txt": [None, "Received Solar Radiation Flux (w/sq m)"],
+                        "Heat_TR.txt": [None, "Thermal Radiation Flux (w/sq m)"],
+                        "Hyd_DA.txt": [None, "Ave Depth (m)"],
+                        "Hyd_DM.txt": [None, "Max Depth (m)"],
+                        "Hyd_Flow.txt": [None, "Flow Rate (cms)"],
+                        "Hyd_Froude.txt": [None, "Densiometric Froude"],
+                        "Hyd_Hyp.txt": [None, "Hyporheic Exchange (cms)"],
+                        "Hyd_Vel.txt": [None, "Flow Velocity (m/s)"],
+                        "Hyd_WT.txt": [None, "Top Width (m)"],
+                        "Rate_Evap.txt": [None, "Evaporation Rate (mm/hr)"],
+                        "Shade.txt": [None, "Effective Shade"],
+                        "Temp_H20.txt": [None, "Stream Temperature (*C)"],
+                        "Temp_Sed.txt": [None, "Sediment Temperature (*C)"],
+                        "VTS.txt": [None, "View to Sky"]
                     }
 
         for key in self.files.iterkeys():
-            self.files[key] = open(IniParams.OutputDirectory + key, 'w')
-            self.files[key].write("Heat Source Hourly Output File:\n\n")
-            self.files[key].write("Datetime".ljust(14))
+            self.files[key][0] = open(IniParams.OutputDirectory + key, 'w')
+            self.files[key][0].write("Heat Source Hourly Output File:")
+            self.files[key][0].write(self.files[key][1])
+            self.files[key][0].write("\n\n")
+            self.files[key][0].write("Datetime".ljust(14))
             for node in self.reach:
                 aaa = "%0.3f" % node.km
-                self.files[key].write(aaa.ljust(14))
+                self.files[key][0].write(aaa.ljust(14))
                 if node == self.reach[-1]:
-                    self.files[key].write("\n")
+                    self.files[key][0].write("\n")
 
 
     def __del__(self):
@@ -101,10 +105,10 @@ class Output(object):
         for key in variables.iterkeys():
             if node == self.reach[0]:
                 Excel_time = "%0.6f" % (TheTime.toordinal() - 693594 + (TheTime.hour +  (TheTime.minute + TheTime.second / 60) / 60 ) / 24)
-                self.files[key].write(Excel_time.ljust(14))
+                self.files[key][0].write(Excel_time.ljust(14))
             dataf = "%0.3f" % variables[key]
-            self.files[key].write(dataf.ljust(14))
+            self.files[key][0].write(dataf.ljust(14))
             if node == self.reach[-1]:
-                self.files[key].write("\n")
-            self.files[key].flush()
+                self.files[key][0].write("\n")
+            self.files[key][0].flush()
 
